4ef9836323de72ae1ea4f642b8acbfc5ee07b370
Kevin Liu <kevin@potatofrom.space>
2017-09-30 16:16:32 -0400

    Add voice recognition with google

--- akira/__main__.py
+++ akira/__main__.py
@@ -1,10 +1,11 @@
 __version__ = '0.0.1'
 
-import os, math
+import os, math, time, re
 from voice import tts
 from cogs import duck_search, weather, twitter
 import pocketsphinx
 import pyaudio
+import speech_recognition as sr
 
 p = pyaudio.PyAudio()
 stream = p.open(
@@ -15,16 +16,16 @@ stream = p.open(
    frames_per_buffer=20480)
 stream.start_stream()
 
-def wait_for_hotword():
-    pocketsphinx_dir = os.path.dirname(pocketsphinx.__file__)
-    model_dir = os.path.join(pocketsphinx_dir, 'model')
-    config = pocketsphinx.Decoder.default_config()
-    config.set_string('-hmm', os.path.join(model_dir, 'en-us'))
-    config.set_string('-keyphrase', 'hello')
-    config.set_string('-dict', os.path.join(model_dir, 'cmudict-en-us.dict'))
-    config.set_float('-kws_threshold', math.pow(10, -7.5))
-    decoder = pocketsphinx.Decoder(config)
+pocketsphinx_dir = os.path.dirname(pocketsphinx.__file__)
+model_dir = os.path.join(pocketsphinx_dir, 'model')
+config = pocketsphinx.Decoder.default_config()
+config.set_string('-hmm', os.path.join(model_dir, 'en-us'))
+config.set_string('-keyphrase', 'hello')
+config.set_string('-dict', os.path.join(model_dir, 'cmudict-en-us.dict'))
+config.set_float('-kws_threshold', math.pow(10, -9))
+decoder = pocketsphinx.Decoder(config)
 
+def wait_for_hotword():
     decoder.start_utt()
 
     while True:
@@ -38,9 +39,22 @@ def wait_for_hotword():
 
 def main():
     tts("Starting up akira.")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        while wait_for_hotword():
+            tts("Hi!")
+            time.sleep(0.5)
+            audio = r.listen(source)
+            try:
+                statement = str(r.recognize_google(audio))
+            except:
+                tts("Sorry, please try again.")
+                continue
+            statement = re.sub('^(hi |hello )', '', statement)
+            tts(statement)
 
-    while wait_for_hotword():
-        tts("Hi!")
+            time.sleep(4)
 
         #weather.run()
 
