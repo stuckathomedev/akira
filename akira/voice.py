d0e41b2e874fd3cd31de3521e3246aa5236c107d
Kevin Liu <kevin@potatofrom.space>
2017-09-30 11:16:32 -0400

    Speed up voice

--- akira/voice.py
+++ akira/voice.py
@@ -1,7 +1,7 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-engine.setProperty('rate', 150)  #120 words per minute
+engine.setProperty('rate', 165)
 engine.setProperty('volume', 0.9)
 engine.setProperty('voice', 'english-north')
 
