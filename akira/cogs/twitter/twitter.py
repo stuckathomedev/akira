6e664534875e490cbf5435a9fb86e2907688ebd4
안잋 읻일아 <aneeshedara@gmail.com>
2017-09-30 12:02:36 -0400

    Finished Twitter Functionality

---
+++ akira/cogs/twitter/twitter.py
@@ -0,0 +1,14 @@
+import tweepy
+from cogs.twitter.twitter_cfg import cfg
+
+def get_api(cfg):
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+    return tweepy.API(auth)
+
+def send_tweet(tweet_contents):
+    api = get_api(cfg)
+    tweet = tweet_contents
+    status = api.update_status(status=tweet)
+
+send_tweet("Hello World!")
