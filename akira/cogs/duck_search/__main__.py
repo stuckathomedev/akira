c0d543ee28be220ed2103901d7ce31a53ef6d033
안잋 읻일아 <aneeshedara@gmail.com>
2017-09-30 12:13:48 -0400

    Fixed all __main__ files

--- akira/cogs/duck_search/__main__.py
+++ akira/cogs/duck_search/__main__.py
@@ -1,16 +1,4 @@
-import urllib
-import json
+from cogs.duck_search.ddg_instant_search import ddg_instant_answer
 
-import requests
-from lxml import html
-
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-
-def ddg_instant_answer(query):
-    urlencoded_query = urllib.parse.quote_plus(query)
-    r = requests.get("http://api.duckduckgo.com/?q=" + urlencoded_query +"&format=json&pretty=1",
-                     headers={'User-Agent': USER_AGENT})
-
-    jdata = r.json()
-    print(jdata["RelatedTopics"][0]["Text"])
-
+def main(search_query):
+    ddg_instant_answer(search_query)
