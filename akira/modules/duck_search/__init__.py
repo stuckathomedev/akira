9b43ac55d993c29e57a6c0e4ae982db948efa55b
Kevin Liu <kevin@potatofrom.space>
2017-09-30 21:26:19 -0400

    Fix duck search

--- akira/modules/duck_search/__init__.py
+++ akira/modules/duck_search/__init__.py
@@ -2,7 +2,7 @@ import re
 from modules.duck_search.ddg_instant_search import ddg_instant_answer
 from voice import tts
 
-trigger_regex = re.compile('^(?:please )?search for$')
+trigger_regex = re.compile('^search for (.+)$')
 
 def run(search_query):
     tts(f"Your results: {ddg_instant_answer(search_query.groups()[0])}")
